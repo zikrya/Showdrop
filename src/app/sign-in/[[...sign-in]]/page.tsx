fc979a7d20e9b04b18bad2c929cb257f1adfc2c8
zikrya <zikrya8@gmail.com>
2025-03-04 21:27:58 -0500

    chore(ui) center clerk components

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,17 @@
-import { SignIn } from '@clerk/nextjs'
-
+import { SignIn } from "@clerk/nextjs";
 export default function Page() {
-  return <SignIn path='/sign-in' />
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div>
+        <SignIn
+          path="/sign-in"
+          appearance={{
+            elements: {
+              formButtonPrimary: "bg-slate-500 hover:bg-slate-400 text-sm normal-case",
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 }
